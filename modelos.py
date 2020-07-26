8c9101810410e24de8f8264a35196f6fc3918625
EdvarFilho <edvarfilho1234@gmail.com>
2020-07-26 15:23:39 -0300

    [+]Resultados Prévios e Alteração do Split

--- modelos.py
+++ modelos.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 from math import log, pi, sqrt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import svm
-from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -18,11 +19,14 @@ warnings.filterwarnings("ignore")
 
 
 # Função que separa o dataset em treino e teste
-def train_testSplit(ids, data, rate_test):
+def train_testSplit(ids, dataPCA, dataModSULQ, rate_test):
     quantidadeIds = len(ids)
     quantidadeTeste = int(rate_test * quantidadeIds)
     idsTreino, idsTeste = [], []
+<<<<<<< HEAD
+=======
     
+>>>>>>> b03c31e52adb626f17c287e8fb8d984c548e67a9
     while(len(idsTeste)!=quantidadeTeste):
         idEscolhido = random.choice(ids)
         if(idEscolhido not in idsTeste):
@@ -31,6 +35,30 @@ def train_testSplit(ids, data, rate_test):
     for i in range(0, quantidadeIds):
         if(ids[i] not in idsTeste):
             idsTreino.append(ids[i])
+<<<<<<< HEAD
+    trainPCA, testPCA, trainModSULQ, testModSULQ = [], [], [], []
+    for idTreino in idsTreino:
+        dataIdPCA = dataPCA.query('id == '+str(idTreino))
+        dataIdModSULQ = dataModSULQ.query('id == '+str(idTreino))
+        for reg in dataIdPCA.index:
+            trainPCA.append(dataIdPCA.loc[reg])
+        for reg in dataIdModSULQ.index:
+            trainModSULQ.append(dataIdModSULQ.loc[reg])
+    for idTeste in idsTeste:
+        dataIdPCA = dataPCA.query('id == '+str(idTeste))
+        dataIdModSULQ = dataModSULQ.query('id == '+str(idTeste))
+        for reg in dataIdPCA.index:
+            testPCA.append(dataIdPCA.loc[reg])
+        for reg in dataIdModSULQ.index:
+            testModSULQ.append(dataIdModSULQ.loc[reg])
+    trainPCA = np.array(trainPCA)
+    testPCA = np.array(testPCA)
+    trainModSULQ = np.array(trainModSULQ)
+    testModSULQ = np.array(testModSULQ)
+    trainTestPCA = trainPCA[:,1:-1], testPCA[:,1:-1], trainPCA[:,-1], testPCA[:,-1]
+    trainTestModSULQ = trainModSULQ[:,1:-1], testModSULQ[:,1:-1], trainModSULQ[:,-1], testModSULQ[:,-1]
+    return trainTestPCA, trainTestModSULQ
+=======
             
     train, test = [], []
     for idTreino in idsTreino:
@@ -46,6 +74,7 @@ def train_testSplit(ids, data, rate_test):
     train = np.array(train)
     test = np.array(test)
     return train[:,1:-1], test[:,1:-1], train[:,-1], test[:,-1]
+>>>>>>> b03c31e52adb626f17c287e8fb8d984c548e67a9
 
 # Função que computa a função logística (sigmóide)
 def sigmoide(row, w):
@@ -53,9 +82,47 @@ def sigmoide(row, w):
     return yPred
 
 # Função responsável para treinar o modelo de Regressão Logística via Gradiente Descendente
-def fitRL(x, y, n_epochs, alpha, fatorReg):
-    print("[Regressão Logística] Treinando modelo...")
+def fitRL(x, y):
+    print("[Regressão Logística] Selecionando hiperparâmetros...")
 
+    epochs = [100, 1000, 10000]
+    alphas = [0.001, 0.01, 0.1, 1]
+    fatoresReg = [0.01, 0.1, 1]
+    
+    valoresF1 = np.zeros((len(epochs), len(alphas), len(fatoresReg)))
+
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size = 0.25)
+    
+    auxTrain = np.ones((x_train.shape[0], 1))
+    auxVal = np.ones((x_val.shape[0], 1))
+    x_train = np.hstack((auxTrain, x_train))
+    x_val = np.hstack((auxVal, x_val))
+    
+    for e in range(0, len(epochs)):
+        for a in range(0, len(alphas)):
+            for f in range(0, len(fatoresReg)):
+                wPrev = np.zeros(x_train.shape[1])
+                for ep in range (0, epochs[e]):
+                    suma = 0
+                    for i in range(0, x_train.shape[0]):
+                        suma = suma +  (y_train[i]-sigmoide(x_train[i],wPrev))*np.transpose(x_train[i])
+                    wPrevReg = fatoresReg[f]*np.transpose(wPrev)
+                    wPrevReg[0] = wPrevReg[0] / fatoresReg[f]
+                    w = np.transpose(wPrev) + alphas[a]*((1/x_train.shape[0])*suma - wPrevReg)
+                    wPrev = w
+                yPredito = []
+                for row in x_val:
+                    if(sigmoide(row, w)>=0.5):
+                        yPredito.append(1)
+                    else:
+                        yPredito.append(0)
+                valoresF1[e,a,f] = f1_score(y_val, yPredito, average='macro')
+    
+    best_params = np.unravel_index(np.argmax(valoresF1, axis=None), valoresF1.shape)
+    n_epochs, alpha, fatorReg = epochs[best_params[0]], alphas[best_params[1]], fatoresReg[best_params[2]]
+    
+    print("[Regressão Logística] Treinando modelo...")
+    
     erroQM = []
     wPrev = np.zeros(x.shape[1]+1)
     aux = np.ones((x.shape[0], 1))
@@ -72,30 +139,47 @@ def fitRL(x, y, n_epochs, alpha, fatorReg):
         erroEp = ((1/(2*x.shape[0]))*sumErro)
         erroQM.append(erroEp)
         wPrev = w
+        
+    print("[Regressão Logística] Hiperparâmetros escolhidos para Regressão Logística: {'n_epochs': "+str(n_epochs)+", 'alpha': "+str(alpha)+", 'lambda': "+str(fatorReg)+"}")
+
     return wPrev, erroQM
 
 # Função responsável para predizer os dados
 def predictRL(w, x):
     print("[Regressão Logística] Testando modelo...")
+<<<<<<< HEAD
+    aux = np.ones((x.shape[0], 1))
+    x = np.hstack((aux, x))
+    yPredito = [1 if(sigmoide(row, w)>=0.5) else 0 for row in x]
+=======
 
     aux = np.ones((x.shape[0], 1))
     x = np.hstack((aux, x))
     
     yPredito = [1 if(sigmoide(row, w)>=0.5) else 0 for row in x]
 
+>>>>>>> b03c31e52adb626f17c287e8fb8d984c548e67a9
     return yPredito
 
 # Função responsável para "treinar" que gera os dados estatísticos necessários para o modelo de 
 # Análise de Discriminante Gaussiano
 def fitAGD(x, y):
+<<<<<<< HEAD
+    print("[Análise de Discriminante Gaussiano] Treinando modelo...")
+=======
     print("[Análise Discriminante Gaussiano] Treinando modelo...")
+>>>>>>> b03c31e52adb626f17c287e8fb8d984c548e67a9
     classes, occurrences = np.unique(y, return_counts=True)
     numClasses = len(classes)
     n = len(y)
     numFeatures = x.shape[1]
     
     probabilidadeClasses = dict(zip(classes, occurrences))
+<<<<<<< HEAD
+
+=======
    
+>>>>>>> b03c31e52adb626f17c287e8fb8d984c548e67a9
     for key in probabilidadeClasses:
         probabilidadeClasses[key] = probabilidadeClasses[key] / n
     
@@ -126,7 +210,7 @@ def predict1AGD(model, row):
     
 # Função utilizada para predizer as classes de um conjunto de registros
 def predictAGD(model, x_test):
-    print("[Análise Discriminante Gaussiano] Testando modelo...")
+    print("[Análise de Discriminante Gaussiano] Testando modelo...")
     yPredito = np.array([predict1AGD(model, row) for row in x_test])
     return yPredito
 
@@ -140,9 +224,13 @@ def distance_manhattan(x1, x2):
 # Função responsável por predizer a classe de um único registro
 def predict1KNN(x, y, x_teste, k, function):
     classes = np.unique(y)
+<<<<<<< HEAD
+    results = [[function(x[i], x_teste), y[i]] for i in range(0, x.shape[0])]
+=======
     
     results = [[function(x[i], x_teste), y[i]] for i in range(0, x.shape[0])]
 
+>>>>>>> b03c31e52adb626f17c287e8fb8d984c548e67a9
     results = sorted(results)
     dictClasses = {}
     for i in classes:
@@ -179,7 +267,7 @@ def predictKNN(x, y, x_test, function):
     return yPredito
 
 # Função responsável por treinar a Árvore de Decisão e escolher os melhores hiperparâmetros por meio de grid-search
-def fitAD(x, y, criterion, max_depth):
+def fitAD(x, y):
     print("[Árvore de Decisão] Selecionando hiperparâmetros...")
     listMaxDepth = range(1, 50)
     configTree = {'criterion':['gini','entropy'],'max_depth':listMaxDepth}
@@ -202,9 +290,14 @@ def predictAD(tree, x, y, x_test):
 # Função responsável por treinar o SVM e escolher os melhores hiperparâmetros por meio de grid-search
 def fitSVM(x, y):
     print("[SVM] Selecionando hiperparâmetros...")
+<<<<<<< HEAD
+    configSVM = [{'kernel': ['rbf'], 'C': 2 ** np.array([-1.0, -2.0, -3.0, 0, 1, 2]), 'gamma': 2 ** np.array([-3.0, -2.0, -1.0])},
+                 {'kernel':['poly'], 'C': 2 ** np.array([-1.0, -2.0, -3.0, 0, 1, 2]),'degree': np.array([3,4,5])}]
+=======
     configSVM = [{'kernel': ['rbf'], 'C': 2 ** np.arange(-5.0, 16.0, 2), 'gamma': 2 ** np.arange(-15.0, 4.0, 2)},
                  {'kernel':['poly'], 'C': 2 ** np.arange(-5.0, 16.0, 2),'degree': np.arange(2, 6)},
                  {'kernel': ['linear'], 'C': 2 ** np.arange(-5.0, 16.0, 2)}]
+>>>>>>> b03c31e52adb626f17c287e8fb8d984c548e67a9
 
     model = GridSearchCV(SVC(), configSVM)
  
@@ -247,8 +340,8 @@ def predictRF(randomForest, x, y, x_test):
 # Função responsável por treinar o Processo Gaussiano e escolher o melhor kernel por meio de grid-search
 def fitGP(x, y):
     print("[Processo Gaussiano] Selecionando kernel...")
-    parameters = {'kernel':(RBF(1.0),Matern(length_scale=1.0, nu=1.5), RationalQuadratic(length_scale=1.0, alpha=1.5), ExpSineSquared(length_scale=1, periodicity=1))}
-   
+    parameters = {'kernel':(RBF(1.0),Matern(length_scale=1.0, nu=1.5), RationalQuadratic(length_scale=1.0, alpha=1.5))}
+
     clf = GridSearchCV(GaussianProcessClassifier(), parameters)
 
     print("[Processo Gaussiano] Treinando modelo...")
